b40992bd1f2614835b8e748fa71b2a81d5d6a662
rocith25 <rocith25@gmail.com>
2026-02-06 13:33:18 +0530

    Create Tribonacci Number 3.c

---
+++ Tribonacci Number 3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+     int a=0,b=1,c = 1,d = 0;
+    for(int i = 2;i<n;i++){
+        d = a + b + c;
+        a = b;
+        b = c;
+        c = d;
+    }
+    return d;
+}
