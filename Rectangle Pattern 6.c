715b9436bdb06f45077c70ef9d903f54a12d301f
rocith25 <rocith25@gmail.com>
2026-02-06 14:19:45 +0530

    Create Rectangle Pattern 6.c

---
+++ Rectangle Pattern 6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int main() {
+    int rows, cols;
+    
+    scanf("%d %d",&rows,&cols);
+
+    for (int i = 1; i <= rows; i++) {
+           
+        
+         for (int j = 1; j <= cols; j++) {
+             
+         printf("*");
+         }
+        printf("\n");
+    }
+
+    return 0;
+}  
+   
